d028d80c4859e1fc30835dce491c6f58c817df94
Adrian Hermo <ahermoabalde@danielcastelao.org>
2024-12-13 10:22:57 +0100

    Este es el readme para enseñarte mi ordinograma

---
+++ examen.java
@@ -0,0 +1,7 @@
+public class examen {
+    /**
+     * Un ordinograma que me recorre un tablero de 8x8 de damas  sacando por pantalla si la casilla está ocupada (mostrando la coordenada) y que tipo de ficha es.
+     * @author Adrian Hermo
+     * @version 1.0
+     */
+}
